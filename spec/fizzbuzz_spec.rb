3e550424ea506ac9d2131d15088d3806271c01af
yevdyko <evgeny.dyko@gmail.com>
2015-10-08 23:58:37 +0300

    added second test

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,4 +4,8 @@ describe 'fizzbuzz' do
   it 'We return Fizz when our number divided by 3 without remain' do
     expect(fizzbuzz(3)).to eq 'Fizz'
   end
+describe 'fizzbuzz' do
+  it 'We return Buzz when our number divided by 5 without remain' do
+    expect(fizzbuzz(5)).to eq 'Buzz'
+  end
 end
