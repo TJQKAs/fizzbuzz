9293215d7de952732c38713b412dfcc45ea5fa1e
yevdyko <evgeny.dyko@gmail.com>
2015-10-08 23:48:15 +0300

    add fizzbuzz.rb

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,6 +1,7 @@
-require 'Fizzbuzz'
-describe Fizzbuzz do
-  it "We return Fizz when our number divided by 3 without remain" do
-    expect(fizzbuzz(3)).to eq "Fizz"
+require 'fizzbuzz'
+
+describe 'fizzbuzz' do
+  it 'We return Fizz when our number divided by 3 without remain' do
+    expect(fizzbuzz(3)).to eq 'Fizz'
   end
 end
