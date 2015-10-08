62d27a150b935e6cec0cbbed682c658592203e19
TJQKAs <a26052013@gmail.com>
2015-10-08 22:21:29 +0100

    third test past

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,4 +1,5 @@
 def fizzbuzz(number)
+  return "Fizzbuzz" if number%3 == 0 && number%5 == 0
   return "Fizz" if number%3 == 0
   return "Buzz" if number%5 == 0
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -7,12 +7,7 @@ describe 'fizzbuzz' do
   it 'We return Buzz when our number divided by 5 without remain' do
     expect(fizzbuzz(5)).to eq 'Buzz'
   end
-<<<<<<< HEAD
-end
-end
-=======
   it 'We return Fizzbuzz when our number divided by 5 and by 3 without remain' do
     expect(fizzbuzz(15)).to eq 'Fizzbuzz'
   end
 end
->>>>>>> 1d966d4f653dcb3f0937d38f3fd6e6d6734f0ad2
